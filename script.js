9c1fd8d726d4c3d03019a64add95ba1718361bd8
Yotty0404 <tyuke9216@hogmail.co.jp>
2024-07-01 21:11:27 +0900

    wakeLockの書き方変更

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //スクリーンロック防止
-navigator.wakeLock.request("screen");
+let wakeLock = await navigator.wakeLock.request('screen');
 
 
 var ajaxBaseUrlOfNationalHolidays = "https://api.national-holidays.jp/";
